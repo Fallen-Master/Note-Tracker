4b31118c713f4d0df26a15e915e72f5efa429e4a
Rafael Gomez <rgomez2014@gmail.com>
2023-09-04 10:26:30 -0500

    removed extra code

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 const express = require('express');
-const fs = require('fs');
-const uuid = require('uuid');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
-const app = express();
 
+const app = express();
+const notesRoutes = require('./routes/notesRoutes');
+
+// Middleware to handle JSON requests
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
+
+// Serve static files
 app.use(express.static('public'));
 
-let notes = []
+// Use notesRoutes for API routes
+app.use('/api/notes', notesRoutes);
 
+// HTML routes
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-  });
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-  
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// Catch-all for any other routes, redirect to homepage
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
